16b4562cba3bccd9107e40d9594f4b26494ebb72
starjrm00 <starjrm00@gmail.com>
2021-01-23 06:00:28 +0000

    add Note's classname

--- src/PianoWithRecording.js
+++ src/PianoWithRecording.js
@@ -69,44 +69,74 @@ class PianoWithRecording extends React.Component {
       img.alt = "asdf";
       if(48 === midiNumber){
         img.className = "C3"
+      } else if(49 === midiNumber){
+        img.className = "CS3"
       } else if(50 === midiNumber){
         img.className = "D3"
+      } else if(51 === midiNumber){
+        img.className = "DS3"
       } else if(52 === midiNumber){
         img.className = "E3"
       } else if(53 === midiNumber){
         img.className = "F3"
+      } else if(54 === midiNumber){
+        img.className = "FS3"
       } else if(55 === midiNumber){
         img.className = "G3"
+      } else if(56 === midiNumber){
+        img.className = "GS3"
       } else if(57 === midiNumber){
         img.className = "A3"
+      } else if(58 === midiNumber){
+        img.className = "AS3"
       } else if(59 === midiNumber){
         img.className = "B3"
       } else if(60 === midiNumber){
         img.className = "C4"
+      } else if(61 === midiNumber){
+        img.className = "CS4"
       } else if(62 === midiNumber){
         img.className = "D4"
+      } else if(63 === midiNumber){
+        img.className = "DS4"
       } else if(64 === midiNumber){
         img.className = "E4"
       } else if(65 === midiNumber){
         img.className = "F4"
+      } else if(66 === midiNumber){
+        img.className = "FS4"
       } else if(67 === midiNumber){
         img.className = "G4"
+      } else if(68 === midiNumber){
+        img.className = "GS4"
       } else if(69 === midiNumber){
         img.className = "A4"
+      } else if(70 === midiNumber){
+        img.className = "AS4"
       } else if(71 === midiNumber){
         img.className = "B4"
       } else if(72 === midiNumber){
         img.className = "C5"
+      } else if(73 === midiNumber){
+        img.className = "CS5"
       } else if(74 === midiNumber){
         img.className = "D5"
+      } else if(75 === midiNumber){
+        img.className = "DS5"
       } else if(76 === midiNumber){
         img.className = "E5"
       } else if(77 === midiNumber){
         img.className = "F5"
+      } else if(78 === midiNumber){
+        img.className = "FS5"
       } else if(79 === midiNumber){
         img.className = "G5"
+      } else if(80 === midiNumber){
+        img.className = "GS5"
       } else if(81 === midiNumber){
         img.className = "A5"
+      } else if(82 === midiNumber){
+        img.className = "AS5"
       } else if(83 === midiNumber){
         img.className = "B5"
       }
