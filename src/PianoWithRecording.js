e87f6d4e737f04b3d2dfa7a4f7dbeaed5223f10a
heejaykong <heejay.dev@gmail.com>
2021-01-23 21:11:21 +0900

    func: show sharp note when black key is pressed

--- src/PianoWithRecording.js
+++ src/PianoWithRecording.js
@@ -5,27 +5,45 @@ import './styles/test.css';
 // const DURATION_UNIT = 0.2;
 // const DEFAULT_NOTE_DURATION = DURATION_UNIT;
 
+
+// const BLACK_KEYS_MIDI_NUMBER = [49, 51, 54, 56, 58, 61, 63, 66, 68, 70, 73, 75, 78, 80, 82];
+
 let NoteHeight = {};
 NoteHeight[48] = 5.6
+NoteHeight[49] = 5.6
 NoteHeight[50] = 5.1
+NoteHeight[51] = 5.1
 NoteHeight[52] = 4.6
 NoteHeight[53] = 4.1
+NoteHeight[54] = 4.1
 NoteHeight[55] = 3.6
+NoteHeight[56] = 3.6
 NoteHeight[57] = 3.1
+NoteHeight[58] = 3.1
 NoteHeight[59] = 2.6
 NoteHeight[60] = 2.1
+NoteHeight[61] = 2.1
 NoteHeight[62] = 1.6
+NoteHeight[63] = 1.6
 NoteHeight[64] = 1.1
 NoteHeight[65] = 0.6
+NoteHeight[66] = 0.6
 NoteHeight[67] = 0.1
+NoteHeight[68] = 0.1
 NoteHeight[69] = -0.45
+NoteHeight[70] = -0.45
 NoteHeight[71] = -0.95
 NoteHeight[72] = -1.55
+NoteHeight[73] = -1.55
 NoteHeight[74] = -2.1
+NoteHeight[75] = -2.1
 NoteHeight[76] = -2.53
 NoteHeight[77] = -3.1
+NoteHeight[78] = -3.1
 NoteHeight[79] = -3.6
+NoteHeight[80] = -3.6
 NoteHeight[81] = -4.1
+NoteHeight[82] = -4.1
 NoteHeight[83] = -4.6
 
 class PianoWithRecording extends React.Component {
@@ -80,6 +98,9 @@ class PianoWithRecording extends React.Component {
   };
 
   recordNotes = (midiNumbers, duration, noteType) => {
+    let isBlackKey;
+    const BLACK_KEYS_MIDI_NUMBER = [49, 51, 54, 56, 58, 61, 63, 66, 68, 70, 73, 75, 78, 80, 82];
+    
     if (this.props.recording.mode !== "RECORDING") {
       return;
     }
@@ -89,24 +110,36 @@ class PianoWithRecording extends React.Component {
       const parentSpan = document.querySelector(".musicNotePrint");
       const span = document.createElement("span");
       const img = new Image();
-      img.src = require(noteType + ".png");
-      img.alt = `musical note(${noteType})`;
+
+      isBlackKey = BLACK_KEYS_MIDI_NUMBER.includes(midiNumber);
+      if (isBlackKey) {
+        img.src = require(noteType + "_w_sharp.png");
+        img.alt = `musical note with sharp(${noteType})`;
+      } else {
+        img.src = require(noteType + ".png");
+        img.alt = `musical note(${noteType})`;
+      }
       img.style.position = "relative";
-      img.style.top = NoteHeight[midiNumber]+"em";
+      img.style.top = `${NoteHeight[midiNumber]}em`;
       switch (noteType) {
         case "./img/Whole_note" :
+        case "./img/Whole_note_w_sharp" :
           img.style.marginRight = "8em";
           break;
         case "./img/Half_note" :
+        case "./img/Half_note_w_sharp" :
           img.style.marginRight = "4em";
           break;
         case "./img/Quarter_note" :
+        case "./img/Quarter_note_w_sharp" :
           img.style.marginRight = "2em";
           break;
         case "./img/Eighth_note" :
+        case "./img/Eighth_note_w_sharp" :
           img.style.marginRight = "1em";
           break;
         default: // "./img/Sixteenth_note" :
+        // "./img/Sixteenth_note_w_sharp" :
           img.style.marginRight = "0em";
       }
       // if(/* 어쩌구면 */){
